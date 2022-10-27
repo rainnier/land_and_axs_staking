151b287189e1254b9efe6f15f936a2b61afb587d
rain <rainnier@gmail.com>
2022-10-28 03:09:57 +0800

    resetting after 24 hours buffer

--- cron_land_claim_axs_stake_restake.js
+++ cron_land_claim_axs_stake_restake.js
@@ -10,41 +10,60 @@ var ms = 0
 
 const claimStakeAndRestakeAllWithSleep = () => {
   fs.readFile('counter.txt', 'utf8', async (err, data) => {
-    console.log('Triggered cron at ' + new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' }))
+    console.log(
+      'Triggered cron at ' +
+        new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' })
+    )
     multiplier = data
     ms = multiplier * (1000 * secondsWait)
-    var waitClaimToCompleteMs = 30*1000 // 30 seconds
-    var waitStakeToCompleteMs = 20*60*1000 // 20 minutes
+    var waitClaimToCompleteMs = 30 * 1000 // 30 seconds
+    var waitStakeToCompleteMs = 20 * 60 * 1000 // 20 minutes
 
-    console.log('Will wait for ' + ms / 1000 + ' seconds before claiming')
-    await new Promise(r => setTimeout(r, ms))
-    console.log('Claiming now at ' + new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' }))
-    claimAll()
+    if (+multiplier === 72) {
+      // Reached 24 hours (72 times of 20 minutes)
+      // Reset multiplier to 0 to ignore current cron trigger and trigger the next 1 for the whole 24 hours
+      multiplier = 0
+      fs.writeFile('counter.txt', '' + multiplier, () => {
+        console.log('Resetting multiplier now to ' + multiplier)
+      })
+    } else {
+      multiplier++
+      fs.writeFile('counter.txt', '' + multiplier, () => {
+        console.log('Updated multiplier now to ' + multiplier)
+      })
 
-    console.log(
-      'Will wait for ' +
-        waitClaimToCompleteMs / 1000 +
-        ' seconds before staking land rewards'
-    )
-    await new Promise(r => setTimeout(r, waitClaimToCompleteMs))
-    console.log(
-      'Staking AXS land rewards now at ' + new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' })
-    )
-    stakeAllBalance()
+      console.log('Will wait for ' + ms / 1000 + ' seconds before claiming')
+      await new Promise(r => setTimeout(r, ms))
+      console.log(
+        'Claiming now at ' +
+          new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' })
+      )
+      claimAll()
 
-    console.log(
-      'Will wait for ' +
-        waitStakeToCompleteMs / 1000 +
-        ' seconds before restaking'
-    )
-    await new Promise(r => setTimeout(r, waitStakeToCompleteMs))
-    console.log('Restaking AXS now at ' + new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' }))
-    restakeAll()
+      console.log(
+        'Will wait for ' +
+          waitClaimToCompleteMs / 1000 +
+          ' seconds before staking land rewards'
+      )
+      await new Promise(r => setTimeout(r, waitClaimToCompleteMs))
+      console.log(
+        'Staking AXS land rewards now at ' +
+          new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' })
+      )
+      stakeAllBalance()
 
-    multiplier++
-    fs.writeFile('counter.txt', '' + multiplier, () => {
-      console.log('Updated multiplier now to ' + multiplier)
-    })
+      console.log(
+        'Will wait for ' +
+          waitStakeToCompleteMs / 1000 +
+          ' seconds before restaking'
+      )
+      await new Promise(r => setTimeout(r, waitStakeToCompleteMs))
+      console.log(
+        'Restaking AXS now at ' +
+          new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila' })
+      )
+      restakeAll()
+    }
   })
 }
 
